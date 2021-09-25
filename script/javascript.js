20e5edb582d3c5fbdf906a50770329c7eb1d610b
Bhushan Mendhe <bhushanmendhe15@gmail.com>
2021-09-25 14:59:51 +0530

    quiz html updated

--- script/javascript.js
+++ script/javascript.js
@@ -12,7 +12,7 @@ function isTriangleEventHandler() {
     angle1 = parseInt(angle1.value);
     angle2 = parseInt(angle2.value);
     angle3 = parseInt(angle3.value);
-    console.log(angle1 + angle2 + angle3);
+
     if((angle1 + angle2 + angle3)===180) {
         showMessageIsTriangle.innerText = "Yayy, the angles form a triangle";
     } else {
@@ -21,3 +21,12 @@ function isTriangleEventHandler() {
 }
 
 //page 2 Quiz Javascript
+
+var q1Option1 = document.querySelector("#q1-option1");
+var q1Option2 = document.querySelector("#q1-option2");
+var q1Option3 = document.querySelector("#q1-option3");
+var q2Option1 = document.querySelector("#q2-option1");
+var q2Option2 = document.querySelector("#q2-option2");
+var q2Option3 = document.querySelector("#q2-option3");
+
+
