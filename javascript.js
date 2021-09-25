68e90812ee430fbc56ad37ea1ca066c39413fb4a
Bhushan Mendhe <bhushanmendhe15@gmail.com>
2021-09-25 11:07:37 +0530

    is Triangle JS complete

---
+++ javascript.js
@@ -0,0 +1,23 @@
+//page 1 Is Triangle javascript
+
+var angle1 = document.querySelector("#angle1");
+var angle2 = document.querySelector("#angle2");
+var angle3 = document.querySelector("#angle3");
+var btnCheckTriangle = document.querySelector("#btn-check-triangle");
+var showMessageIsTriangle = document.querySelector("#show-message-is-Triangle");
+
+btnCheckTriangle.addEventListener("click",isTriangleEventHandler);
+
+function isTriangleEventHandler() {
+    angle1 = parseInt(angle1.value);
+    angle2 = parseInt(angle2.value);
+    angle3 = parseInt(angle3.value);
+    console.log(angle1 + angle2 + angle3);
+    if((angle1 + angle2 + angle3)===180) {
+        showMessageIsTriangle.innerText = "Yayy, the angles form a triangle";
+    } else {
+        showMessageIsTriangle.innerText = "Oh Oh!, the angles do not form a triangle"
+    }
+}
+
+//page 2 Quiz Javascript
